cb510f09b2c7a99bf0165691f2628672b37d82e4
GalaXy11232 <neamtumatei08@gmail.com>
2026-03-28 22:56:37 +0200

    cheeky description update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/webhook', async (req, res) => {
             const messageParts = commit.message.split('\n\n');
             const commitTitle = messageParts[0].trim();
  
-            const commitDescription = "- " + (messageParts.slice(1).join('\n\n').trim() || "_No description provided._");
+            const commitDescription = (messageParts.slice(1).join('\n\n').trim() || "_No description provided._");
             
             return {
                 name: `"${commitTitle}" (\`${commit.author.name}\`)`,
